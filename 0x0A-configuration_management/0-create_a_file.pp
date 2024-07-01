6f48b9753a90882a118f03cb2c3d27b9f316ae30
Patrick <patricknwosu360@gmail.com>
2024-07-01 12:40:52 +0100

    NEW

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # This manifest creates a file at /tmp
 file{ '/tmp/school':
 ensure  => 'present',
-content => 'I love puppet',
+content => 'I love Puppet',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
